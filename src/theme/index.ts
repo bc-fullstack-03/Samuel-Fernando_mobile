02df5456eb4811a06c69d05a57f018d56a4ba863
SamuelFST <guti.samuel2002@gmail.com>
2023-05-31 00:02:03 -0300

    chore: theme file

---
+++ src/theme/index.ts
@@ -0,0 +1,21 @@
+export const THEME = {
+  COLORS: {
+    BACKGROUND_900: '#121214',
+    BACKGROUND_600: '#202024',
+    TEXT: '#EEEEEE',
+    CAPTION_400: '#7C7C8A',
+    CYAN_300: '#4DD0E1',
+  },
+  FONTS: {
+    REGULAR: 'Inter_400Regular',
+    SEMIBOLD: 'Inter_600SemiBold',
+    BOLD: 'Inter_700Bold',
+    BLACK: 'Inter_900Black',
+  },
+  FONT_SIZES: {
+    SM: 14,
+    MD: 16,
+    LG: 24,
+    XLG: 30,
+  },
+};
