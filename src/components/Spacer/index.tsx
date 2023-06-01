72ad560c42d623f5a2bd9a75d323a5f1e82c23ac
SamuelFST <guti.samuel2002@gmail.com>
2023-05-31 22:32:16 -0300

    feat: spacer component

---
+++ src/components/Spacer/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+interface SpacerProps {
+  children?: ReactNode;
+}
+
+function Spacer({children}: SpacerProps) {
+  return (
+    <View style={styles.container}>
+      {children}
+    </View>
+  );
+}
+
+export default Spacer;
