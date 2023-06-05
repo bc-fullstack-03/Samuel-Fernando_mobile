6debe38fee6f8c5e2faff741893d273d52c42807
SamuelFST <guti.samuel2002@gmail.com>
2023-06-05 10:16:07 -0300

    feat: auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,136 @@
+import React, { ReactNode, useReducer } from 'react';
+import jwtDecode from 'jwt-decode';
+import * as SecureStore from 'expo-secure-store';
+
+import { Auth, UserToken } from '../model/Auth';
+import api from '../services/api';
+
+interface AuthContext {
+  token: string | null;
+  user: string | null;
+  profile: string | null;
+  errorMessage: string | null;
+  isLoading: boolean;
+  login?: () => void;
+  tryLocalLogin?: () => void;
+  register?: () => void;
+}
+
+const defaultValue = {
+  token: null,
+  user: null,
+  profile: null,
+  errorMessage: null,
+  isLoading: true,
+};
+
+const Context = React.createContext<AuthContext>(defaultValue);
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'login':
+        return {
+          ...state,
+          ...action.payload,
+          errorMessage: '',
+        };
+
+      case 'auth_error':
+        return {
+          ...state,
+          errorMessage: action.payload,
+          isLoading: false,
+        };
+
+      case 'user_created':
+        return {
+          ...state,
+          ...action.payload,
+          errorMessage: '',
+        };
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, defaultValue);
+
+  const login = async (auth: Auth) => {
+    try {
+      const { data } = await api.post('/authentication', auth);
+      const decodedToken = jwtDecode(data.token) as UserToken;
+      const userProfile = await api.get(`/profile/${decodedToken.sub}`, {
+        headers: {
+          Authorization: `Bearer ${data.token}`,
+        },
+      });
+
+      await SecureStore.setItemAsync('accessToken', data.token);
+      await SecureStore.setItemAsync('profile', JSON.stringify(userProfile.data));
+      await SecureStore.setItemAsync('userId', decodedToken.sub);
+
+      dispatch({
+        type: 'login',
+        payload: {
+          token: data.token,
+          profile: userProfile.data,
+          userId: data.userId,
+          isLoading: false,
+        },
+      });
+    } catch (err) {
+      dispatch({
+        type: 'auth_error',
+        payload: 'Ocorreu um erro na autenticação',
+      });
+    }
+  };
+
+  const tryLocalLogin = async () => {
+    try {
+      const [token, profile, userId] = await Promise.all([
+        await SecureStore.getItemAsync('accessToken'),
+        await SecureStore.getItemAsync('profile'),
+        await SecureStore.getItemAsync('userId'),
+      ]);
+
+      dispatch({
+        type: 'login',
+        payload: {
+          token,
+          profile,
+          userId,
+          isLoading: false,
+        },
+      });
+    } catch (err) {
+
+    }
+  }
+
+  const register = async (auth: Auth) => {
+    try {
+      await api.post('/user', auth);
+      dispatch({
+        type: 'user_created',
+        isLoading: false,
+      });
+    } catch (err) {
+      console.log(err);
+      dispatch({
+        type: 'auth_error',
+        payload: 'Ocorreu um erro na criação da conta',
+      });
+    }
+  }
+
+  return (
+    <Context.Provider value={{...state, login, tryLocalLogin, register}}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export { Provider, Context };
