2015b2f8901f5a60c362ce23c0a3c0f7728e2394
SamuelFST <guti.samuel2002@gmail.com>
2023-06-07 11:51:57 -0300

    feat: function to create a post

--- src/context/PostContext.tsx
+++ src/context/PostContext.tsx
@@ -4,12 +4,21 @@ import api from '../services/api';
 import { Post } from '../model/Post';
 import { generateAuthHeader } from '../services/auth';
 import { Context as AuthContext } from './AuthContext';
+import { navigate } from '../../RootNavigation';
+import { File } from '../model/File';
 
 interface PostContext {
   posts: Post[];
   getPosts?: () => Post[];
   likePost?: (postId: string) => void;
   unlikePost?: (postId: string) => void;
+  createPost?: (newPost: PostCreate, file: File) => void;
+}
+
+interface PostCreate {
+  title: string;
+  description: string;
+  isImage: boolean;
 }
 
 const defaultValue = {
@@ -30,6 +39,13 @@ const Provider = ({ children }: { children: ReactNode }) => {
           ...state,
           posts: action.payload,
         };
+
+      case 'post_create': {
+        return {
+          ...state,
+          posts: [action.payload, ...state.posts],
+        };
+      }
 
       case 'post_like': {
         const index = posts.findIndex(
@@ -79,6 +95,42 @@ const Provider = ({ children }: { children: ReactNode }) => {
     }
   }
 
+  const createPost = async (newPost: PostCreate, file: any) => {
+    const postData = new FormData();
+
+    if (file) {
+      postData.append('photo', file);
+      newPost.isImage = true;
+    } else {
+      postData.append('photo', null);
+    }
+
+    try {
+      await api.post(
+        `/post?title=${newPost.title}&description=${newPost.description}&isImage=${newPost.isImage}`,
+        postData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+          transformRequest: (data, headers) => {
+            return postData;
+          },
+        });
+      const { data } = await api.get('/post', generateAuthHeader(token));
+
+      dispatch({
+        type: 'post_create',
+        payload: data[data.length - 1],
+      });
+
+      navigate('Posts');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   const likePost = async (postId: string) => {
     try {
       await api.post(`/post/${postId}/like`, null, generateAuthHeader(token));
@@ -106,7 +158,7 @@ const Provider = ({ children }: { children: ReactNode }) => {
   }
 
   return (
-    <Context.Provider value={{ ...state, getPosts, likePost, unlikePost }}>
+    <Context.Provider value={{ ...state, getPosts, likePost, unlikePost, createPost }}>
       {children}
     </Context.Provider>
   );
